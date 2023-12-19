f166d2b13c23131ed3c504fa9e809f3cad32def5
JaisonPhilipJacob <jaisonpjacob811@gmail.com>
2023-12-19 20:31:18 +0530

    Update Template

--- playground/views.py
+++ playground/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return HttpResponse('Hello World')
+    # return HttpResponse('Hello World')
+    return render(request, "hello.html", {"name" : "Jaison"})
+    # return render(request) - Will fail since the template name is required
